bca351e94e6ac7c0c90cf4ec388413593b60ce36
lucas ciannella <lucasciannello@gmail.com>
2023-04-04 14:23:45 -0300

    docker-compose configs and application.yml

---
+++ src/main/java/com/gym/controlapp/ControlappApplication.java
@@ -0,0 +1,13 @@
+package com.gym.controlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/controlapp/ControlappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.controlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
